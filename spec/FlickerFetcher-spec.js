896fddba5a2a9769dd5baee8affa781dd9434126
Torstein Nicolaysen <torstein.nicolaysen@bekk.no>
2011-07-08 09:59:32 +0200

    Rewrote the LuckyFlicker object to be self initializing

--- spec/FlickerFetcher-spec.js
+++ spec/FlickerFetcher-spec.js
@@ -4,13 +4,12 @@ describe("Flicker result fetcher", function() {
 		it("should do a search with a given keyword against flickr", function() {
 			var keyword = "BEKK",
 				outputElementId = "testDomElement",
-				luckyFlickr = new jstdd.LuckyFlickr(outputElementId),
 				fetcher = new jstdd.FlickrFetcher();
 
 			spyOn(fetcher, "getResult");
 
-			luckyFlickr.setFetcher(fetcher);
-			luckyFlickr.search(keyword);
+			jstdd.LuckyFlickr.fetcher = fetcher;
+			jstdd.LuckyFlickr.search(keyword);
 
 			expect(fetcher.getResult).toHaveBeenCalledWith(keyword);
 		});
