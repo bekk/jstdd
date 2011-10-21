e8085d6badce2cd821c505e94603d8b49cf379fc
Torstein Nicolaysen <torstein.nicolaysen@bekk.no>
2011-10-21 16:53:27 +0200

    Improved the design on FlickrResultParser

--- src/FlickrResultParser.js
+++ src/FlickrResultParser.js
@@ -5,23 +5,28 @@ var jstdd = jstdd || {}; // Ensure that the namespace exist
  * functions to read the data-structure.
  */
 jstdd.FlickrResultParser = function(searchResult) {
-	this.searchResult = searchResult;
+    // Instance fields
+    var _searchResult = searchResult;
 
-	this.getFirstResult = function() {
-		return this.searchResult.items[0];
-	}
+    // Private methods
+    function getFirstResult() {
+        return _searchResult.items[0];
+    }
+
+    // Public API
+    return {
+        /**
+         * The only way to read the markup from the result. We only
+         * need the first result.
+         *
+         * Handles empty result-sets by returning a "no result" message.
+         */
+        getMarkup: function() {
+            if (!_searchResult || _searchResult.length === 0) {
+                return "No results found";
+            }
+
+            return getFirstResult().description;
+        }
+    }
 };
-
-/**
- * The only way to read the markup from the result. We only
- * need the first result.
- *
- * Handles empty result-sets by returning a "no result" message.
- */
-jstdd.FlickrResultParser.prototype.getMarkup = function() {
-	if (!this.searchResult || this.searchResult.length === 0) {
-		return "No results found";
-	}
-
-	return this.getFirstResult().description;
-}
